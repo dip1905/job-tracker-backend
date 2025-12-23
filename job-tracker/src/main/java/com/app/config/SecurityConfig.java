c0febd66c7b8e9a92bcd11dbe7cafb68957e58de
dip1905 <dipesh.patel9977@gmail.com>
2025-12-23 16:54:20 +0530

    Update SecurityConfig.java

--- job-tracker/src/main/java/com/app/config/SecurityConfig.java
+++ job-tracker/src/main/java/com/app/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import com.app.security.JwtAuthFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
